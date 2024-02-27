287d070da1b2f07834fe26ca4ee0df4265780572
jirbthagoras <hansjabriel@gmail.com>
2024-02-27 10:15:31 +0700

    menmabahkan sistem checkbox

--- helper/connection.php
+++ helper/connection.php
@@ -5,7 +5,7 @@ function getConnection(): PDO
     $host = "localhost";
     $database = "u842353793_bukuTamu";
     $username = "u842353793_adminRoot";
-    $password = "";
+    $password = "gc]emU|r6^B";
 
     return new PDO("mysql:host=$host;dbname=$database", $username, $password);
 }
